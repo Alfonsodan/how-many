34546346f56e9d33d34e3de04d7f2b45d3431755
Daniel Colangelo <Danalfons@yahoo.com>
2017-02-21 23:18:12 -0500

    guess the number
    a simple guessing game where you guess a number between 0 and 10 and
    the app tells you if your write or wrong.

---
+++ how many/how many/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  how many
+//
+//  Created by Danny Colangelo on 2/21/17.
+//  Copyright © 2017 Danny Colangelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var enterGuessBox: UITextField!
+    @IBOutlet weak var answerBox: UILabel!
+    @IBAction func guessButton(_ sender: Any) {
+        
+        let rand = Int(arc4random_uniform(11))
+        let guess = Int(enterGuessBox.text!)
+        if rand == guess {
+            answerBox.text = "You got it!"
+        } else if  rand != guess {
+            answerBox.text = "Sorry! The number was \(rand)"
+        }
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
